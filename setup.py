8826fa9d9d741d3db2bff3afe45bf7d98e665122
valeriozhang <valeriozhang14@gmail.com>
2020-12-15 00:45:01 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-version = "1.0.3rc3"
+version = "1.0.3rc4"
 
 with open("docs/About.rst", "r") as fh:
     long_description = fh.read()
@@ -41,8 +41,8 @@ setup(
     zip_safe=False,
     install_requires=[
         "setuptools",
-        "valer.core==1.3.5rc3",
-        "valer.jsonapi==1.2.3rc3",
+        "valer.core==1.3.5rc4",
+        "valer.jsonapi==1.2.3rc4",
     ],
     extras_require={
         "test": [
